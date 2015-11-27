f871e411aa488885a1fc1e4ea6e7989fbc068b06
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-11-27 10:20:42 -0500

    [simple_test] Add two GripperCommand states

--- behaviors/youbot_behavior_simple_test/src/youbot_behavior_simple_test/simple_test_sm.py
+++ behaviors/youbot_behavior_simple_test/src/youbot_behavior_simple_test/simple_test_sm.py
@@ -11,9 +11,10 @@ from flexbe_core import Behavior, Autonomy, OperatableStateMachine, Logger
 from flexbe_states.log_state import LogState
 from youbot_flexbe_states.execute_arm_trajectory_state import ExecuteTrajectoryState
 from flexbe_navigation_states.move_base_state import MoveBaseState
+from youbot_flexbe_states.gripper_state import GripperCommandState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
-
+from geometry_msgs.msg import Pose2D
 # [/MANUAL_IMPORT]
 
 
@@ -47,10 +48,12 @@ class SimpleTestSM(Behavior):
 	def create(self):
 		front_pose_traj = [[0.0, 1.1094, -4.0187, 1.789, 0.0]]
 		traj_time = [10.0]
-		target_pose = [1.0, 1.0, 45.0] # [m, m, degrees]
-		origin_pose = [0.0, 0.0, 0.0]  # [m, m, degrees]
-		# x:582 y:424, x:458 y:265
+		target_pose = Pose2D(1.0, 1.0, 45.0) # [m, m, degrees]
+		origin_pose = Pose2D(0.0, 0.0, 0.0)  # [m, m, degrees]
+		# x:950 y:88, x:441 y:221
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.target_pose = target_pose
+		_state_machine.userdata.origin_pose = origin_pose
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -59,29 +62,43 @@ class SimpleTestSM(Behavior):
 
 
 		with _state_machine:
-			# x:129 y:68
+			# x:138 y:67
 			OperatableStateMachine.add('Announce_Execution_Start',
 										LogState(text="Execution is starting!", severity=Logger.REPORT_INFO),
-										transitions={'done': 'Move_Forward_and_Turn'},
+										transitions={'done': 'Open_Gripper'},
 										autonomy={'done': Autonomy.Low})
 
-			# x:137 y:421
+			# x:637 y:79
 			OperatableStateMachine.add('Move_Arm_Forward',
 										ExecuteTrajectoryState(target_pose=front_pose_traj, time=traj_time),
 										transitions={'done': 'finished', 'failed': 'failed'},
 										autonomy={'done': Autonomy.Low, 'failed': Autonomy.Low})
 
-			# x:131 y:189
+			# x:140 y:415
 			OperatableStateMachine.add('Move_Forward_and_Turn',
-										MoveBaseState(target_pose=target_pose),
-										transitions={'arrived': 'Move_to_Origin', 'failed': 'failed'},
-										autonomy={'arrived': Autonomy.Low, 'failed': Autonomy.Low})
+										MoveBaseState(),
+										transitions={'arrived': 'Close_Gripper', 'failed': 'failed'},
+										autonomy={'arrived': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'waypoint': 'target_pose'})
 
-			# x:143 y:298
+			# x:651 y:415
 			OperatableStateMachine.add('Move_to_Origin',
-										MoveBaseState(target_pose=origin_pose),
+										MoveBaseState(),
 										transitions={'arrived': 'Move_Arm_Forward', 'failed': 'failed'},
-										autonomy={'arrived': Autonomy.Low, 'failed': Autonomy.Low})
+										autonomy={'arrived': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'waypoint': 'origin_pose'})
+
+			# x:147 y:213
+			OperatableStateMachine.add('Open_Gripper',
+										GripperCommandState(gripper_cmd=GripperCommandState.OPEN_GRIPPER, time=5.0),
+										transitions={'done': 'Move_Forward_and_Turn', 'failed': 'failed'},
+										autonomy={'done': Autonomy.Low, 'failed': Autonomy.Low})
+
+			# x:435 y:415
+			OperatableStateMachine.add('Close_Gripper',
+										GripperCommandState(gripper_cmd=GripperCommandState.CLOSE_GRIPPER, time=5.0),
+										transitions={'done': 'Move_to_Origin', 'failed': 'failed'},
+										autonomy={'done': Autonomy.Low, 'failed': Autonomy.Low})
 
 
 		return _state_machine
