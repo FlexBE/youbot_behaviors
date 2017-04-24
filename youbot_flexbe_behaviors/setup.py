1d56e1bdc8fd14ed48131a648ea87374dea2f354
Philipp Schillinger <philsplus@gmail.com>
2017-04-24 21:55:32 +0200

    Convert repo to new flexbe_app

--- youbot_flexbe_behaviors/setup.py
+++ youbot_flexbe_behaviors/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['behavior_example_behavior'],
+    packages = ['youbot_flexbe_behaviors'],
     package_dir = {'': 'src'}
 )
 
